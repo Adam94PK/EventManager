b14e1b713f0b038aef6103f29561b8128a5df2a4
Adam94PK <adamzadora94@gmail.com>
2016-01-03 20:11:57 +0100

    add association to models

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,5 @@
 class Event < ActiveRecord::Base
+	has_and_belongs_to_many :guests
+	belongs_to :user
+	has_one :main_page
 end
--- app/models/main_page.rb
+++ app/models/main_page.rb
@@ -1,2 +1,3 @@
 class MainPage < ActiveRecord::Base
+	belongs_to :event
 end
