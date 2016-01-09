0efcb418028ff0b281529211f3065846b662aec6
Adam94PK <adamzadora94@gmail.com>
2016-01-09 22:02:18 +0100

    refactor test

--- test/controllers/events_controller_test.rb
+++ test/controllers/events_controller_test.rb
@@ -2,15 +2,17 @@ require 'test_helper'
 
 class EventsControllerTest < ActionController::TestCase
   
+  setup :initialize_events
+
   test "should get index" do
     get :index
     assert_response :success
-    assert_template :index
-    assert_equal((assigns(:events)),[@one, @two] )
+    assert_template :index 
+    assert_equal (assigns(:events)), Event.all
   end
 
   test "should get show" do
-    get :show, id: @one.id
+    get :show, id: @user1.events[Random.rand(21)]
     assert_response :success
   end
 
@@ -20,7 +22,7 @@ class EventsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit , id: @one.id
+    get :edit , id: @user1.events[Random.rand(21)]
     assert_response :success
   end
 
@@ -33,30 +35,32 @@ class EventsControllerTest < ActionController::TestCase
 
 
   test "should get only asocieted events when user is signed_in" do
-    user1 = create(:user_with_events, events_count: 4)
-    user2 = create(:user_with_events, events_count: 10)
-    sign_in user1
+    sign_in @user1
     get :show_followed
     assert_response :success
-    assert_equal((assigns(:events)), user1.events )
-    assert_not_includes(user1.events, user2.events.first)
+    assert_equal((assigns(:events)), @user1.events )
+    assert_not_includes (assigns(:events)), @user2.events.first
   end
 
   test "should get no events when user is not signed_in" do
-    user1 = create(:user_with_events, events_count: 4)
-    user2 = create(:user_with_events, events_count: 10)
     get :show_followed
     assert_response :success
     assert_nil(assigns(:events))
   end
 
   test "shuld redirect destroy when user in not signed_in" do
-    user = create(:user_with_events, events_count: 20)
-    event = user.events[Random.rand(21)]
+    event = @user1.events[Random.rand(21)]
     assert_no_difference 'Event.count' do
       delete :destroy, id: event
     end
     assert_redirected_to new_user_session_path
+  end
+
+  private
+
+  def initialize_events
+    @user1 = create(:user_with_events, events_count: 20)
+    @user2 = create(:user_with_events, events_count: 10)
   end
 
 end
