fd272dc13cc75ad460062e8dae02cd5db7f576da
Adam94PK <adamzadora94@gmail.com>
2016-01-27 13:13:32 +0100

    add crud to agenda

--- app/controllers/agendas_controller.rb
+++ app/controllers/agendas_controller.rb
@@ -1,2 +1,65 @@
 class AgendasController < ApplicationController
+	
+	def new
+	  @event = find_event
+	  @event.build_agenda
+	end
+
+	def show
+	  @agenda = find_event.agenda
+	end
+
+	def create
+	  @event = find_event
+	  @agenda = @event.build_agenda(agenda_params)
+	  puts "ZAJEBISTA Agena _______________________"
+	  puts @agenda.inspect
+	  if @agenda.save
+	  	redirect_to event_agenda_path(@event.id, @agenda)
+	  else
+	  	flash.now[:danger] = "Unpermited paramters"
+	  	render 'new'
+	  end
+	end
+
+	def edit
+		@event = find_event
+	end
+
+	def update
+	  @agenda = find_event.agenda
+	  if @agenda.update(agenda_params)
+	  	redirect_to event_agenda_path(@agenda.event.id, @agenda)
+	  else
+	  	flash.now[:danger] = "Unpermited paramters"
+	  	render 'edit'
+	  end
+	end
+
+	def destroy
+	  @agenda = find_agenda
+	  @event = @agenda.event
+      if current_user == @event.user
+		@agenda.destroy
+		redirect_to @event
+	  else
+		flash[:dange] = "You are not allowd to do that"
+	  end
+	end
+
+	private
+
+	def agenda_params
+		params.require(:agenda).permit(:content, :picture)
+	end
+
+	def find_agenda
+		Agenda.find(params[:id])
+	end
+
+	def find_event
+		Event.find(params[:event_id])
+	end
+
+
 end
