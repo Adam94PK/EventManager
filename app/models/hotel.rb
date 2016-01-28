fb1028211b4b9164e7932e322ed0838c1cb4fd9a
perfectsound <duska94@gmail.com>
2016-01-28 00:34:56 +0000

    change hotels & hotel_pic views

--- app/models/hotel.rb
+++ app/models/hotel.rb
@@ -1,4 +1,6 @@
 class Hotel < ActiveRecord::Base
+	has_attached_file :picture, styles: { medium: "600x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 	has_and_belongs_to_many :events
 	belongs_to :user
 	has_many :hotel_pictures
