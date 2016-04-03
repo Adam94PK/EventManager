837d04a69ca62ebc85aa793b0f230c1ea64e0f0b
Adam94PK <adamzadora94@gmail.com>
2016-04-03 17:03:56 +0200

    add searching user to admin panel

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -3,7 +3,8 @@ class AdminPanelController < ApplicationController
   end
 
   def user_list
-    @users = User.order(:user_name).paginate :page => params[:page], :per_page => 5
+    @temp = params
+    @users = User.where(find_user_by).order(:user_name).paginate :page => params[:page], :per_page => 5
   end
   
   def user_content
@@ -11,5 +12,13 @@ class AdminPanelController < ApplicationController
     @events = @user.events
     @hotels = @user.hotels
   end
-  
+
+  private
+
+  def find_user_by
+    if params[:keyword].present?
+      "#{params[:select_by]} LIKE '#{params[:keyword]}'"
+    end
+  end
+
 end
