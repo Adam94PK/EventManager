ffa980cc0a7831afb9995b2ce2bcafa9e407891b
perfectsound <duska94@gmail.com>
2016-04-03 14:28:14 +0000

    add user filtration to admin panel

--- app/controllers/admin_panel_controller.rb
+++ app/controllers/admin_panel_controller.rb
@@ -3,13 +3,21 @@ class AdminPanelController < ApplicationController
   end
 
   def user_list
-    @users = User.order(:user_name).paginate :page => params[:page], :per_page => 5
+    @users = User.where(filtr_by).order(:user_name).paginate :page => params[:page], :per_page => 5
   end
   
   def user_content
     @user = User.find(params[:id])
     @events = @user.events
     @hotels = @user.hotels
+  end
+  
+  private
+  
+  def filtr_by
+    if params[:role].present?
+      "role LIKE '#{params[:role]}'"
+    end
   end
   
 end
